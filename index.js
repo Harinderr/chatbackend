31fd2c2106febf7d0b051b3509b1382f77c441db
Harinder <nexttoeveryone428@gmail.com>
2023-06-13 15:34:15 +0530

    mongo Uri added

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(cookieparser());
 
 async function connectToDB() {
   try {
-    mongoose.connect("mongodb://127.0.0.1:27017/userDB", {
+    mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
